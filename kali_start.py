3c8abdd7aeee81dba7722f66b99a469f40fdadcf
mikewhiteman <michael.s.whiteman@gmail.com>
2020-12-28 21:07:09 -0500

    Updated kali configuration script

--- kali_start.py
+++ kali_start.py
@@ -4,47 +4,24 @@ import string
 
 ec2 = boto3.resource('ec2')
 
-
-
 def generate_creds():
     alphabet = string.ascii_letters + string.digits
     username = 'cloudkali'
-    password = ''.join(secrets.choice(alphabet) for i in range(8))
-    
+    password = ''.join(secrets.choice(alphabet) for i in range(12))
     return username, password
-
-
-
     
 def create_instance(username, password):
 
-    userdata = """#!/bin/bash
+    userdata = f"""#!/bin/bash
     ###
-    # This script will automatically generate accounts, provision random passwords, and add the accounts t>
+    # This script will provision an an account with a random password
     ###
-
-    function rand_pass {
-    head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 ; echo ''
-    }
-
-    usernames=("cat" "dog" "mouse" "llama" "horse")
-    echo "***Connection Information***"
-    echo "IP Address: $(curl http://169.254.169.254/latest/meta-data/public-ipv4)" 
-
-    for i in "${usernames[@]}"
-    do
-        username=$i
-        randompw=$(rand_pass)
-        useradd -m -s /bin/bash $username
-        echo $username:$randompw |  chpasswd
-        usermod -aG sudo $username
-        echo "User:" $username
-        echo "Password:" $randompw 
-    done
+    useradd -m -s /bin/bash {username}
+    echo {username}:{password} |  chpasswd
+    usermod -aG sudo {username}
     """
 
-
-    instances = ec2.create_instances(
+    ec2_instance = ec2.create_instances(
         ImageId='ami-0f24d5a5c0b5c0a18',
         MinCount=1,
         MaxCount=1,
@@ -55,11 +32,9 @@ def create_instance(username, password):
         SubnetId='subnet-021de55c66cc45ff3'
     ) 
 
-    return instances
-
-
+    return ec2_instance
 
 if __name__ == "__main__":
     username, password = generate_creds()
     instance = create_instance(username,password)
-    print(instance)
+    print(f'[Debug] Created instance {instance} with username: {username} and password of {password}')
