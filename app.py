b9230b6fc3158e06c32fdca9c4358c4db880f340
mikewhiteman <michaelswhiteman@gmail.com>
2021-01-01 21:46:42 -0500

    Updated template

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ login_manager.init_app(app)
 
 @app.route('/') 
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     (app.run(host='0.0.0.0', debug=True))
